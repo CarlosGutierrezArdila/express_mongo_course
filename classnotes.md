ea610435aeb658fd460431f3636acfe872214ecc
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-04-30 14:41:39 -0500

    express simple rest

--- classnotes.md
+++ classnotes.md
@@ -5,3 +5,32 @@
 - var rect = require('./rectangle'); to import in the main file
 # Node Modules: Callbacks and Error Handling
 # Introduction to Express.js
+# REST (Representational State Transfer)
+- interoperability between systems on the network
+- Use web standars
+- Exchange data using XML or JSON
+- Simpler compared with SOAP or WSDL etc
+- Use HTTP methods, Be stateless, Expose directory structured like URIs
+- **Nouns**: naming resources with URIs goin from genral to specific **/** 
+- **Verbs**: 
+    - GET -> READ    
+    - post ->create 
+    - put-> update 
+    - delete-> delete
+- Stateless: the server doesn't track the client side state, its a resposibility from the client to track its own state
+# Express Router
+- app.all, app.get app.post app.put app.delete methods
+- **Body parser** require('body-parser')
+- Express Router creates a mini Express application:
+```js 
+var router = express.Router();
+router.use(bodyParser.json();)
+
+router.route('/')
+.all(...)
+.get(...)
+....
+```
+
+
+
--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,55 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan') // morgan to log info from requests
+const bodyParser = require('body-parser') // to decode data from requests
 
 const hostname = 'localhost'
 const port = 3000
 
 const app = express(); // app is going to use express
 app.use(morgan('dev'))
+app.use(bodyParser.json()) // to use bodyparser
 
-app.use(express.static(__dirname+'/public')) // inform express to look into this folder to serve static files
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next() // executed and handle with functions before
+})
+
+// Support operations on /dishes
+
+app.get('/dishes', (req, res, next) => {
+    res.end('will send all dishes') // end response
+})
+app.post('/dishes', (req, res, next) => {
+    res.end('will create: ' + req.body.name + " - " + req.body.description)
+})
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403
+    res.end('PUT not supported')
+})
+app.delete('/dishes', (req, res, next) => {
+    res.end('will delete all dishes')
+})
+
+// Support operations on /dishes:dishId
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('will send dish:'+req.params.dishId) // end response
+})
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403
+    res.end('POST not supported')
+})
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating \n')
+    res.end('Will update dish: ' +req.params.dishId)
+})
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Will delete dish: ' +req.params.dishId)
+})
+
+app.use(express.static(__dirname + '/public')) // inform express to look into this folder to serve static files
 
 app.use((req, res, next) => { // next is optional parameter to use aditional middleware
     res.statusCode = 200
