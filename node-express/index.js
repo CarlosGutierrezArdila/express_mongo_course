40e280c703ead576e6d4b7e135f0dd4e852e1aac
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-04-30 10:49:29 -0500

    Express serve static files with morgan logs

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan') // morgan to log info from requests
 
 const hostname = 'localhost'
 const port = 3000
 
 const app = express(); // app is going to use express
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname+'/public')) // inform express to look into this folder to serve static files
 
 app.use((req, res, next) => { // next is optional parameter to use aditional middleware
-    console.info(req.headers)
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.end('<html><body><h1>This is an express server</h1></body></html>')
