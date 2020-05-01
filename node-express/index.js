3d615db335a154350d26c30530e854c6bd36de38
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-05-01 12:06:19 -0500

    assignment 1
    implementing express router for dishes promotions and leaders

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan') // morgan to log info from requests
 const bodyParser = require('body-parser') // to decode data from requests
 
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promotionRouter = require('./routes/promotionRouter');
 
 
 const hostname = 'localhost'
@@ -12,9 +14,15 @@ const port = 3000
 const app = express(); // app is going to use express
 app.use(morgan('dev'))
 app.use(bodyParser.json()) // to use bodyparser
-
-app.use('/dishes',dishRouter)
-app.use('/dishes/:dishId',dishRouter)
+// routes for dishes
+app.use('/dishes', dishRouter)
+app.use('/dishes/:dishId', dishRouter)
+// routes for promos
+app.use('/promotions', promotionRouter)
+app.use('/promotions/:promotionId', promotionRouter)
+// routes for leaders
+app.use('/leaders', leaderRouter)
+app.use('/leaders/:leaderId', leaderRouter)
 
 
 app.use(express.static(__dirname + '/public')) // inform express to look into this folder to serve static files
